9bbdf83d5975b9499f7f2be062815537adc3dfe7
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 12:32:44 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,7 +9,8 @@ class MP3Importer
   def files
     filename = Dir.children(@path)
     # filename = Dir.glob("*.mp3")
-    edit_names = filename.each{ |file| file.chomp(".mp3")}
+    edit_names = []
+    edit_names << filename.each{ |file| file.chomp(".mp3")}
     edit_names
     # filename
   end
