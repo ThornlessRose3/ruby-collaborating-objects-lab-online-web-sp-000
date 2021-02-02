43b4d616cb9984b10ab114d761e015c30b12337b
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 12:19:21 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
   end
   
   def files
-    filename = Dir.children(@path)
+    filename = Dir.chrdir(@path)
     filename = Dir.glob("*.mP3")
     puts filename
     puts "This is the start "
