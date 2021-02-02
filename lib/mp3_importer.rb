cbf2c4b8a8e3dd3874ed1bf4c77ed2ba32704162
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 11:54:14 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+class MP3Importer
+
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    Dir.chdir(@path)
+    filename = Dir.glob("*.mp3")
+    puts "This is the start "
+    puts filename
+    filename = filename.split(".mp3")
+    filename
+  end
+  
+  def import(list_of_filenames)
+    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  end
+  
+end  
